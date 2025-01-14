3b7c2f8998c9499f205b5dd2988a6a5994c6c5e0
Ozan Batuhan Ceylan <ozan@code2.io>
2025-01-14 20:05:09 +0300

    express config

--- api/index.js
+++ api/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 var indexRouter = require('../routes/index');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use('/', indexRouter);
 
 app.listen(5000, () => console.log("Server ready on port 3000."));
