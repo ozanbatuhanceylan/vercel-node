55f3f60c932d6bfc198f02a719e1110feb361f2f
Ozan Batuhan Ceylan <ozan@code2.io>
2025-02-04 14:21:01 +0300

    init session enpoint updated

--- routes/index.js
+++ routes/index.js
@@ -24,14 +24,21 @@ const createPeakaProject = async (projectName) => {
 
 const initPeakaSession = async (apiKey) => {
   
-  const resp = await fetch(`${process.env.PEAKA_PARTNER_API_BASE_URL}/ui/initSession?${new URLSearchParams({
-    projectId: apiKey
-}).toString()}`,{
-    method: "GET",
+  const resp = await fetch(`${process.env.PEAKA_PARTNER_API_BASE_URL}/ui/initSession`,{
+    method: "POST",
     headers: {
       "Content-Type": "application/json",
       "Authorization": `Bearer ${process.env.PEAKA_PARTNER_API_KEY}`,
     },
+    body: JSON.stringify({
+      projectId: apiKey,
+      theme: "myDark",
+      themeOverride: true,
+      featureFlags: {
+        queries: false,
+        createDataInPeaka: false
+      }
+    })
   });
   return resp.json()
 }
